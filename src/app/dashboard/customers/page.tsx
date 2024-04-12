04925b7835c66a70ad9ec215534dfb971380e98c
Jhonni10 <jhonieripianoscue@gmail.com>
2024-04-12 10:36:38 -0500

    handling errors - C13

--- src/app/dashboard/customers/page.tsx
+++ src/app/dashboard/customers/page.tsx
@@ -1,30 +1,5 @@
-'use client'
-import Notiflix from "notiflix";
-
-const confirmDelete = ()=>{
-  Notiflix.Confirm.show(
-      'Eliminar Producto',
-      '¿Estas seguro que quieres eliminar este producto?',
-      'Eliminar',
-      'Cancelar',
-      function okCb() {
-          alert('eliminado')
-      },
-      function cancelCb() {
-        return
-      },
-      {
-        width: '320px',
-        borderRadius: '8px',
-        titleColor:'black',
-        okButtonBackground:'black',
-        cssAnimationStyle:'zoom',
-
-      },
-    );
-}
 
 
 export default function Page() {
-    return <button onClick={confirmDelete}>customers buttonage</button>;
+    return <p>customers </p>;
   }
